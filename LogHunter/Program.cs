8f3868e9877187f3de089ea9fc34da6c51578cc7
André Carvalho Fonseca <andre.carvalho.fonseca@outsystems.com>
2026-02-12 13:08:27 +0000

    Initial

--- LogHunter/Program.cs
+++ LogHunter/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LogHunter.Menu;
+using LogHunter.Menus;
+using LogHunter.Services;
+
+namespace LogHunter;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        var root = AppContext.BaseDirectory;
+        var session = new SessionState(root);
+
+        IMenu? menu = new MainMenu(session);
+
+        while (menu is not null)
+            menu = await menu.ShowAsync().ConfigureAwait(false);
+    }
+}
